fb84764994026f7bfd7c7f69e2af16e334cb8d6a
DamiOfi <oficinadamianthames@gmail.com>
2024-11-29 19:02:05 -0300

    agregando redux toolkit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
 )
+
+
+
+  
+   
+
---
+++ src/store/index.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+import clienteReducer from '../features/clienteSlice';
+
+export const store = configureStore({
+  reducer: {
+    cliente: clienteReducer,
+  },
+});
+
+export default store;
